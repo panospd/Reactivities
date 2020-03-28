234712a895a685eaeab2638477bc82cd9491f3dd
Panos Anastasiadis <anastapd@gmail.com>
2020-03-28 14:07:29 +0000

    Added CORS in API and installed axios in client

--- API/Startup.cs
+++ API/Startup.cs
@@ -32,6 +32,16 @@ namespace API
                 opt.UseSqlite(Configuration.GetConnectionString("DefaultConnection"));
             });
 
+            services.AddCors(opt =>
+            {
+                opt.AddPolicy("CorsPolicy", policy =>
+                {
+                    policy
+                    .AllowAnyHeader()
+                    .AllowAnyMethod()
+                    .WithOrigins("http://localhost:3000");
+                });
+            });
             services.AddControllers();
         }
 
@@ -53,6 +63,9 @@ namespace API
             {
                 endpoints.MapControllers();
             });
+
+            app.UseCors("CorsPolicy");
+            app.UseMvc();
         }
     }
 }
