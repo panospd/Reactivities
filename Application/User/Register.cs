7f076b114be4e56307ac4919c787b5fc610559b5
Panos Anastasiadis <anastapd@gmail.com>
2020-04-11 20:38:25 +0100

    Added not empty server validation for password

--- Application/User/Register.cs
+++ Application/User/Register.cs
@@ -32,7 +32,7 @@ namespace Application.User
                 RuleFor(x => x.DisplayName).NotEmpty();
                 RuleFor(x => x.Username).NotEmpty();
                 RuleFor(x => x.Email).NotEmpty().EmailAddress();
-                RuleFor(x => x.Password).Password();
+                RuleFor(x => x.Password).NotEmpty().Password();
             }
         }
 
