78928832d6735daf90d29d9fd8de482d97189ff1
Panos Anastasiadis <anastapd@gmail.com>
2020-04-23 17:04:18 +0100

    Fixed data bug for Safari

--- client-app/src/app/common/util/util.ts
+++ client-app/src/app/common/util/util.ts
@@ -2,22 +2,10 @@ import { IActivity, IAttendee } from '../../Models/activity';
 import { IUser } from '../../Models/user';
 
 export const combineDateAndTime = (date: Date, time: Date) => {
-  const timeString = extractTime(time);
-  const dateString = extractDate(date);
+  const dateString = date.toISOString().split('T')[0];
+  const timeString = time.toISOString().split('T')[1];
 
-  return new Date(dateString + ' ' + timeString);
-};
-
-const extractTime = (time: Date) => {
-  return time.getHours() + ':' + time.getMinutes() + ':00';
-};
-
-const extractDate = (date: Date) => {
-  const year = date.getFullYear();
-  const month = date.getMonth() + 1;
-  const day = date.getDate();
-
-  return `${year}-${month}-${day}`;
+  return new Date(dateString + 'T' + timeString);
 };
 
 export const setActivityProps = (activity: IActivity, user: IUser) => {
